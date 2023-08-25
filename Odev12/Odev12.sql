99165969b480c104b14fa61bc6a3ebbbdb0934fe
Alişan <alisanakbaba2003@hotmail.com>
2023-08-25 15:28:53 +0300

    Odev12
    SQL eğitimdeki 12. ödev

---
+++ Odev12/Odev12.sql
@@ -0,0 +1,38 @@
+/* 1-
+SELECT COUNT(*) FROM film
+WHERE length>
+(
+	SELECT AVG(length) FROM film
+);
+*/
+
+/* 2-
+SELECT COUNT(*) FROM film
+WHERE rental_rate = 
+(
+	SELECT MAX(rental_rate) FROM film
+);
+*/
+
+/*3-
+(
+SELECT title,rental_rate FROM film
+WHERE rental_rate = 
+(
+	SELECT MIN(rental_rate) FROM film
+)
+)
+UNION
+(
+SELECT title,replacement_cost FROM film
+WHERE replacement_cost =
+(
+	SELECT MIN(replacement_cost) FROM film
+)
+)
+*/
+/* 4-
+SELECT customer_id,COUNT(*) FROM payment
+GROUP BY customer_id
+ORDER BY COUNT(*) DESC;
+*/
---
+++ Odev12/readme.md
@@ -0,0 +1,6 @@
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.<br>
+<br>
+1. film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?<br>
+2. film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?<br>
+3. film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.<br>
+4. payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
